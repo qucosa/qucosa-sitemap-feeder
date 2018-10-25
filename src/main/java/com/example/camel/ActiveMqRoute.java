82c5a2b396fded72531bec73d1fc303c87930704
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2018-10-25 10:21:07 +0200

    Add route to feed METS/XML to ExistDB

--- src/main/java/com/example/camel/ActiveMqRoute.java
+++ src/main/java/com/example/camel/ActiveMqRoute.java
@@ -2,6 +2,7 @@ package com.example.camel;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.component.kafka.KafkaComponent;
 import org.apache.camel.component.kafka.KafkaConstants;
 import org.springframework.stereotype.Component;
@@ -21,7 +22,6 @@ public class ActiveMqRoute extends RouteBuilder {
         // Transport updated PIDs to Kafka topic
         from("activemq:topic:fedora.apim.updates")
                 .id("fcrepo_updates")
-                .noAutoStartup()
                 .transform(xpath("/atom:entry/atom:summary[@type='text']/text()", String.class)
                         .namespace("atom", "http://www.w3.org/2005/Atom"))
                 .to("kafka:fcrepo_updates");
@@ -34,8 +34,28 @@ public class ActiveMqRoute extends RouteBuilder {
                 .setHeader(Exchange.HTTP_QUERY, simple("pid=${exchangeProperty[pid]}"))
                 .throttle(1)
                 .to("http4://sdvcmr-app03:8080/mets")
+                .convertBodyTo(String.class, "UTF-8")
                 .setHeader(KafkaConstants.KEY, exchangeProperty("pid"))
                 .to("kafka:mets_updates");
 
+        // Send XML to ExistDB
+        from("kafka:mets_updates?groupId=existdb_feeder")
+                .id("existdb_feeder")
+                .noAutoStartup()
+                .setProperty("pid", header(KafkaConstants.KEY))
+                .process(exchange -> {
+                    String pid = exchange.getIn().getHeader(KafkaConstants.KEY, String.class);
+                    exchange.getIn().setHeader(Exchange.HTTP_PATH,
+                            String.format("/db/qucosa/mets/test/%s.mets.xml", pid.replaceFirst(":", "-")));
+                    exchange.setProperty("pid", pid);
+                })
+                .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.PUT))
+                .setHeader(Exchange.CHARSET_NAME, constant("UTF-8"))
+                .setHeader(Exchange.CONTENT_TYPE, constant("application/xml"))
+                .throttle(10)
+                .to("http4://sdvexistdb01:8080/exist/rest" +
+                        "?authUsername=qucosa-ingest" +
+                        "&authPassword=qucosa-ingest")
+                .log("Updated ${header.CamelHttpPath}");
     }
 }
