ad17c490338ce23f627b556ab54498077e6f13a4
dseelig <daniel.seelig@slub-dresden.de>
2020-01-16 13:43:53 +0100

    Remove spring boot annotaion and starter. Added camel main runner.

--- src/main/java/de/qucosa/camel/CamelApplication.java
+++ src/main/java/de/qucosa/camel/CamelApplication.java
@@ -1,12 +1,19 @@
 package de.qucosa.camel;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.camel.main.Main;
 
-@SpringBootApplication
 public class CamelApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CamelApplication.class, args);
+    public static void main(String[] args) throws Exception {
+        Main main = new Main();
+
+        if (args.length > 0) {
+            main.setPropertyPlaceholderLocations("classpath:" + args[0]);
+        } else {
+            main.setPropertyPlaceholderLocations("classpath:application-dev.properties");
+        }
+
+        main.addRouteBuilder(new ActiveMqRoute());
+        main.run();
     }
 }
