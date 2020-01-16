b1fb091e7159f478361d3f01373e81b078df8cae
dseelig <daniel.seelig@slub-dresden.de>
2020-01-16 14:45:11 +0100

    Optimize code. Change kafka component instance. Remove object instances.

--- src/main/java/de/qucosa/camel/ActiveMqRoute.java
+++ src/main/java/de/qucosa/camel/ActiveMqRoute.java
@@ -5,12 +5,10 @@ import de.qucosa.camel.camelprocessors.SetupJsonForBulkDelete;
 import de.qucosa.camel.camelprocessors.SetupJsonForBulkInsert;
 import de.qucosa.camel.camelprocessors.UrlFormatProcessor;
 import de.qucosa.camel.camelprocessors.UrlsetFormatProcessor;
-import de.qucosa.camel.utils.Utils;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.component.kafka.KafkaComponent;
-import org.apache.camel.processor.aggregate.AggregationStrategy;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -35,53 +33,35 @@ public class ActiveMqRoute extends RouteBuilder {
 
     @Override
     public void configure() {
-        UrlsetFormatProcessor urlsetFormatProcessor = new UrlsetFormatProcessor();
-        UrlFormatProcessor urlFormatProcessor = new UrlFormatProcessor();
-        AMQMessageProcessor amqMessageProcessor = new AMQMessageProcessor();
-        AggregationStrategy appendFedoraObjectInfo = new AppendFedoraObjectInfo(tenantShortMap, tenantLongMap);
-        SetupJsonForBulkInsert jsonForBulkInsert = new SetupJsonForBulkInsert();
-        SetupJsonForBulkDelete jsonForBulkDelete = new SetupJsonForBulkDelete();
-
-
-        // setup kafka component with the brokers
-        KafkaComponent kafka;
-
-        kafka = (KafkaComponent) getContext().getComponent("kafka");
-        // in case KafkaComponent is set already (Unit-/Route-Tests)
-        if (Utils.empty(kafka.getBrokers())) {
-            getContext().removeComponent("kafka");
-            kafka.setBrokers("{{kafka.broker.host}}:{{kafka.broker.port}}");
-            getContext().addComponent("kafka", kafka);
-        }
+        KafkaComponent kafka = new KafkaComponent();
+        kafka.setBrokers("{{kafka.broker.host}}");
+        getContext().addComponent("kafka", kafka);
 
         // Transport updated PIDs to Kafka topic
         from("activemq:topic:fedora.apim.update")
                 .routeId(ACTIVEMQ_ROUTE)
                 // XML-to-JSON-mapping of relevant information
-                .process(amqMessageProcessor)
-                .to("kafka:sitemap_feeder?groupId=modifysitemap")
-        ;
+                .process(new AMQMessageProcessor())
+                .to("kafka:sitemap_feeder?groupId=modifysitemap");
 
         // route to update sitemap via pid's (post qucosa-ID's (qucosa:12345) to kafka topic "pidupdate")
         from("kafka:pidupdate?groupId=bulkinsert")
                 .routeId(KAFKA_BULK_INSERT_ROUTE)
                 // set/get method/tenant/pid/encodedpid
-                .process(jsonForBulkInsert)
-                .to("kafka:sitemap_feeder?groupId=modifysitemap")
-        ;
+                .process(new SetupJsonForBulkInsert())
+                .to("kafka:sitemap_feeder?groupId=modifysitemap");
 
         // route to update sitemap via pid's (post qucosa-ID's (qucosa:12345) to kafka topic "pidupdate")
         from("kafka:piddelete?groupId=bulkdelete")
                 .routeId(KAFKA_BULK_DELETE_ROUTE)
                 // set/get method/tenant/pid/encodedpid
-                .process(jsonForBulkDelete)
-                .to("kafka:sitemap_feeder?groupId=modifysitemap")
-        ;
+                .process(new SetupJsonForBulkDelete())
+                .to("kafka:sitemap_feeder?groupId=modifysitemap");
 
         from("kafka:sitemap_feeder?groupId=modifysitemap")
                 .routeId(SITEMAP_FEEDER_ROUTE)
                 // appends tenant (urlset-name) and objectState to JSON-body
-                .enrich("direct:objectinfo", appendFedoraObjectInfo)
+                .enrich("direct:objectinfo", new AppendFedoraObjectInfo(tenantShortMap, tenantLongMap))
                 .id(APPEND_FEDORA_OBJ_INFO)
                 .choice()
                     .when().jsonpath("$.[?(@.objectState == 'I')]").to("mock:inactive_doc")
@@ -104,8 +84,7 @@ public class ActiveMqRoute extends RouteBuilder {
                                 .to("direct:sitemap_modify_url_lastmod")
                                 .id(HTTP_MODIFY_OBJECT_ID)
                         .endChoice()
-                .endChoice()
-        ;
+                .endChoice();
 
         from("direct:objectinfo")
                 .setProperty("encodedpid", jsonpath("$.encodedpid"))
@@ -115,55 +94,50 @@ public class ActiveMqRoute extends RouteBuilder {
         from("direct:sitemap_create_urlset")
                 .routeId("createUrlsetRoute")
                 // set json-format for urlset's with tenantname (urlset-uri)
-                .process(urlsetFormatProcessor)
+                .process(new UrlsetFormatProcessor())
                 .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.POST))
                 .setHeader(Exchange.CHARSET_NAME, constant("UTF-8"))
                 .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
                 .throttle(10)
                 // throwExceptionOnFailure set to false to disable camel from throwing HttpOperationFailedException
                 // on response-codes 300+
-                .recipientList(simple("http4://{{sitemap.host}}:{{sitemap.port}}/urlsets?throwExceptionOnFailure=false"))
-        ;
+                .recipientList(simple("http4://{{sitemap.host}}:{{sitemap.port}}/urlsets?throwExceptionOnFailure=false"));
 
         from("direct:sitemap_create_url")
                 .routeId("createUrlRoute")
                 // create urlset if missing
                 .setProperty("tenant", jsonpath("$.tenant_urlset"))
-                .process(urlFormatProcessor)
+                .process(new UrlFormatProcessor())
                 .log("create_url (json): ${body}")
                 .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.POST))
                 .setHeader(Exchange.CHARSET_NAME, constant("UTF-8"))
                 .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
                 .setHeader("pid", exchangeProperty("pid"))
                 .throttle(10)
-                .recipientList(simple("http4://{{sitemap.host}}:{{sitemap.port}}/urlsets/${exchangeProperty.tenant}"))
-        ;
+                .recipientList(simple("http4://{{sitemap.host}}:{{sitemap.port}}/urlsets/${exchangeProperty.tenant}"));
 
         from("direct:sitemap_delete_urlset")
                 .routeId("deleteUrlsetRoute")
                 .setProperty("tenant", jsonpath("$.tenant_urlset"))
                 .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.DELETE))
-                .recipientList(simple("http4://{{sitemap.host}}:{{sitemap.port}}/urlsets/${exchangeProperty.tenant}?throwExceptionOnFailure=false"))
-        ;
+                .recipientList(simple("http4://{{sitemap.host}}:{{sitemap.port}}/urlsets/${exchangeProperty.tenant}?throwExceptionOnFailure=false"));
 
         from("direct:sitemap_delete_url")
                 .routeId("deleteUrlRoute")
                 .setProperty("tenant", jsonpath("$.tenant_urlset"))
-                .process(urlFormatProcessor)
+                .process(new UrlFormatProcessor())
                 .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.DELETE))
                 .setHeader(Exchange.CHARSET_NAME, constant("UTF-8"))
                 .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-                .recipientList(simple("http4://{{sitemap.host}}:{{sitemap.port}}/urlsets/${exchangeProperty.tenant}/deleteurl?throwExceptionOnFailure=false"))
-        ;
+                .recipientList(simple("http4://{{sitemap.host}}:{{sitemap.port}}/urlsets/${exchangeProperty.tenant}/deleteurl?throwExceptionOnFailure=false"));
 
         from("direct:sitemap_modify_url_lastmod")
                 .routeId("modifyUrlRoute")
                 .setProperty("tenant", jsonpath("$.tenant_urlset"))
-                .process(urlFormatProcessor)
+                .process(new UrlFormatProcessor())
                 .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.PUT))
                 .setHeader(Exchange.CHARSET_NAME, constant("UTF-8"))
                 .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-                .recipientList(simple("http4://{{sitemap.host}}:{{sitemap.port}}/urlsets/${exchangeProperty.tenant}?throwExceptionOnFailure=false"))
-        ;
+                .recipientList(simple("http4://{{sitemap.host}}:{{sitemap.port}}/urlsets/${exchangeProperty.tenant}?throwExceptionOnFailure=false"));
     }
 }
