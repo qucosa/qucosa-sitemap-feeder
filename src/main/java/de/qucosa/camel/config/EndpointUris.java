e83c53f8b90d5413aaea10b1c4f288dba232f1e1
dseelig <daniel.seelig@slub-dresden.de>
2020-01-27 12:34:46 +0100

    Added delete url endpoint uri.

--- src/main/java/de/qucosa/camel/config/EndpointUris.java
+++ src/main/java/de/qucosa/camel/config/EndpointUris.java
@@ -4,5 +4,6 @@ public class EndpointUris {
     public final static String KAFKA_SITEMAP_CONSUMER = "kafka:service_events?groupId=sitemap&consumersCount=1&breakOnFirstError=true&autoOffsetReset=earliest";
     public final static String FEDORA_3_OBJECTINFO = "seda:objectinfo?multipleConsumers=true";
     public final static String DIRECT_CREATE_URL = "direct:sitemap_create_url";
+    public final static String DIRECT_DELETE_URL = "direct:sitemap_delete_url";
     public final static String SITEMAP_SERVICE_CREATE_URL = "http4://{{sitemap.service.url}}/url";
 }
