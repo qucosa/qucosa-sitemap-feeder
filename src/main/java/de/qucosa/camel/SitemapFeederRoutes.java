a18be3918faa89178652968bc1327fa8155a4171
dseelig <daniel.seelig@slub-dresden.de>
2020-01-27 09:25:01 +0100

    Modify kafka service events kafka route. Chnage hardcoded uris with config constants.

--- src/main/java/de/qucosa/camel/SitemapFeederRoutes.java
+++ src/main/java/de/qucosa/camel/SitemapFeederRoutes.java
@@ -1,9 +1,6 @@
 package de.qucosa.camel;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import de.qucosa.camel.camelprocessors.SetupJsonForBulkDelete;
-import de.qucosa.camel.camelprocessors.SetupJsonForBulkInsert;
-import de.qucosa.camel.camelprocessors.UrlFormatProcessor;
 import de.qucosa.camel.model.Tenant;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
@@ -12,10 +9,14 @@ import org.apache.camel.component.kafka.KafkaComponent;
 
 import java.util.List;
 
-import static de.qucosa.camel.utils.RouteIds.APPEND_FEDORA_OBJ_INFO;
-import static de.qucosa.camel.utils.RouteIds.KAFKA_BULK_DELETE_ROUTE;
-import static de.qucosa.camel.utils.RouteIds.KAFKA_BULK_INSERT_ROUTE;
-import static de.qucosa.camel.utils.RouteIds.SITEMAP_FEEDER_ROUTE;
+import static de.qucosa.camel.config.EndpointUris.DIRECT_CREATE_URL;
+import static de.qucosa.camel.config.EndpointUris.FEDORA_3_OBJECTINFO;
+import static de.qucosa.camel.config.EndpointUris.KAFKA_SITEMAP_CONSUMER;
+import static de.qucosa.camel.config.EndpointUris.SITEMAP_SERVICE_CREATE_URL;
+import static de.qucosa.camel.config.RouteIds.APPEND_FEDORA_3_OBJ_INFO;
+import static de.qucosa.camel.config.RouteIds.FEDORA_3_OBJECTINFO_ID;
+import static de.qucosa.camel.config.RouteIds.KAFKA_SITEMAP_CONSUMER_ID;
+import static de.qucosa.camel.config.RouteIds.SITEMAP_CREATE_URL_ID;
 
 public class SitemapFeederRoutes extends RouteBuilder {
 
@@ -25,113 +26,71 @@ public class SitemapFeederRoutes extends RouteBuilder {
         kafka.setBrokers("{{kafka.broker.host}}");
         getContext().addComponent("kafka", kafka);
 
-        // route to update sitemap via pid's (post qucosa-ID's (qucosa:12345) to kafka topic "pidupdate")
-        from("kafka:pidupdate?groupId=bulkinsert")
-                .routeId(KAFKA_BULK_INSERT_ROUTE)
-                // set/get method/tenant/pid/encodedpid
-                .process(new SetupJsonForBulkInsert())
-                .to("kafka:sitemap_feeder");
-
-        // route to update sitemap via pid's (post qucosa-ID's (qucosa:12345) to kafka topic "pidupdate")
-        from("kafka:piddelete?groupId=bulkdelete")
-                .routeId(KAFKA_BULK_DELETE_ROUTE)
-                // set/get method/tenant/pid/encodedpid
-                .process(new SetupJsonForBulkDelete())
-                .to("kafka:sitemap_feeder");
-
-        from("kafka:service_events?groupId=sitemap&consumersCount=1&breakOnFirstError=true&autoOffsetReset=earliest")
-                .routeId(SITEMAP_FEEDER_ROUTE)
-                .enrich("direct:objectinfo", new AppendFedoraObjectInfo(tenants())).id(APPEND_FEDORA_OBJ_INFO)
+        from(KAFKA_SITEMAP_CONSUMER)
+                .routeId(KAFKA_SITEMAP_CONSUMER_ID)
+                .enrich("seda:objectinfo", new AppendFedoraObjectInfo(tenants())).id(APPEND_FEDORA_3_OBJ_INFO)
                 .choice()
                     .when(simple("${property.objectState} == 'A'"))
                         .choice()
-                            .when(simple("${property.eventType} == 'create'"))
+                            .when(simple("${property.eventType} == 'create' or ${property.eventType} == 'update'"))
                                 .to("direct:sitemap_create_url")
+                            .when(simple("${property.eventType} == 'delete'"))
+                                .to("direct:sitemap_delete_url")
                     .otherwise()
                         .to("mock:otherwise");
 
-//        from("kafka:sitemap_feeder?groupId=modifysitemap")
-//                .routeId(SITEMAP_FEEDER_ROUTE)
-//                // appends tenant (urlset-name) and objectState to JSON-body
-//                .enrich("direct:objectinfo", new AppendFedoraObjectInfo(tenants()))
-//                .id(APPEND_FEDORA_OBJ_INFO)
-//                .choice()
-//                    .when().jsonpath("$.[?(@.objectState == 'A')]")
-//                        .choice()
-//                            .when().jsonpath("$.[?(@.method == 'addDatastream')]")
-//                                .to("direct:sitemap_modify_url_lastmod")
-//                                .id(HTTP_ADD_DATASTREAM_ID)
-//                            .when().jsonpath("$.[?(@.method == 'ingest')]")
-//                                .multicast()
-//                                .parallelProcessing(false)
-//                                .to("direct:sitemap_create_urlset", "direct:sitemap_create_url")
-//                                .id(HTTP_INGEST_ID)
-//                                .endChoice() // needed for multicast
-//                            .when().jsonpath("$.[?(@.method == 'purgeObject')]")
-//                                .to("direct:sitemap_delete_url")
-//                                .id(HTTP_PURGE_OBJECT_ID)
-//                            .when().jsonpath("$.[?(@.method == 'modifyObject')]")
-//                                .to("direct:sitemap_modify_url_lastmod")
-//                                .id(HTTP_MODIFY_OBJECT_ID)
-//                        .endChoice()
-//                    .otherwise()
-//                        .to("mock:not_active_doc")
-//                .endChoice();
+        // This route is for fedora 3 only.
+        from(FEDORA_3_OBJECTINFO)
+                .routeId(FEDORA_3_OBJECTINFO_ID)
+//                .setProperty("encodedpid", jsonpath("$.encodedpid"))
+                .recipientList(simple("http4://{{fedora.service.url}}/fedora/objects/$['org.fcrepo.jms.identifier']?format=xml"));
 
-        // This is object by fedora 3 only.
-        from("direct:objectinfo")
-                .setProperty("encodedpid", jsonpath("$.encodedpid"))
-                .recipientList(simple("http4://{{fedora.service.url}}/fedora/objects/${exchangeProperty.encodedpid}?format=xml"));
-
-        // Sitemap update
-//        from("direct:sitemap_create_urlset")
-//                .routeId("createUrlsetRoute")
-//                // set json-format for urlset's with tenantname (urlset-uri)
-//                .process(new UrlsetFormatProcessor())
-//                .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.POST))
-//                .setHeader(Exchange.CHARSET_NAME, constant("UTF-8"))
-//                .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-//                .throttle(10)
-//                // throwExceptionOnFailure set to false to disable camel from throwing HttpOperationFailedException
-//                // on response-codes 300+
-//                .recipientList(simple("http4://{{sitemap.service.url}}/urlsets?throwExceptionOnFailure=false"));
-
-        from("direct:sitemap_create_url")
-                .routeId("createUrlRoute")
-                // create urlset if missing
-                .setProperty("tenant", jsonpath("$.tenant_urlset"))
-                .process(new UrlFormatProcessor())
+        from(DIRECT_CREATE_URL)
+                .routeId(SITEMAP_CREATE_URL_ID)
                 .log("create_url (json): ${body}")
                 .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.POST))
                 .setHeader(Exchange.CHARSET_NAME, constant("UTF-8"))
                 .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-                .setHeader("pid", exchangeProperty("pid"))
                 .throttle(10)
-                .recipientList(simple("http4://{{sitemap.service.url}}/urlsets/${exchangeProperty.tenant}"));
+                .to(SITEMAP_SERVICE_CREATE_URL);
 
-        from("direct:sitemap_delete_urlset")
-                .routeId("deleteUrlsetRoute")
-                .setProperty("tenant", jsonpath("$.tenant_urlset"))
-                .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.DELETE))
-                .recipientList(simple("http4://{{sitemap.service.url}}/urlsets/${exchangeProperty.tenant}?throwExceptionOnFailure=false"));
+        // route to update sitemap via pid's (post qucosa-ID's (qucosa:12345) to kafka topic "pidupdate")
+//        from("kafka:pidupdate?groupId=bulkinsert")
+//                .routeId(KAFKA_BULK_INSERT_ROUTE)
+//                // set/get method/tenant/pid/encodedpid
+//                .process(new SetupJsonForBulkInsert())
+//                .to("kafka:sitemap_feeder");
 
-        from("direct:sitemap_delete_url")
-                .routeId("deleteUrlRoute")
-                .setProperty("tenant", jsonpath("$.tenant_urlset"))
-                .process(new UrlFormatProcessor())
-                .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.DELETE))
-                .setHeader(Exchange.CHARSET_NAME, constant("UTF-8"))
-                .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-                .recipientList(simple("http4://{{sitemap.service.url}}/urlsets/${exchangeProperty.tenant}/deleteurl?throwExceptionOnFailure=false"));
+        // route to update sitemap via pid's (post qucosa-ID's (qucosa:12345) to kafka topic "pidupdate")
+//        from("kafka:piddelete?groupId=bulkdelete")
+//                .routeId(KAFKA_BULK_DELETE_ROUTE)
+//                // set/get method/tenant/pid/encodedpid
+//                .process(new SetupJsonForBulkDelete())
+//                .to("kafka:sitemap_feeder");
 
-        from("direct:sitemap_modify_url_lastmod")
-                .routeId("modifyUrlRoute")
-                .setProperty("tenant", jsonpath("$.tenant_urlset"))
-                .process(new UrlFormatProcessor())
-                .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.PUT))
-                .setHeader(Exchange.CHARSET_NAME, constant("UTF-8"))
-                .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-                .recipientList(simple("http4://{{sitemap.service.url}}/urlsets/${exchangeProperty.tenant}?throwExceptionOnFailure=false"));
+//        from("direct:sitemap_delete_urlset")
+//                .routeId("deleteUrlsetRoute")
+//                .setProperty("tenant", jsonpath("$.tenant_urlset"))
+//                .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.DELETE))
+//                .recipientList(simple("http4://{{sitemap.service.url}}/urlsets/${exchangeProperty.tenant}?throwExceptionOnFailure=false"));
+//
+//        from("direct:sitemap_delete_url")
+//                .routeId("deleteUrlRoute")
+//                .setProperty("tenant", jsonpath("$.tenant_urlset"))
+//                .process(new UrlFormatProcessor())
+//                .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.DELETE))
+//                .setHeader(Exchange.CHARSET_NAME, constant("UTF-8"))
+//                .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+//                .recipientList(simple("http4://{{sitemap.service.url}}/urlsets/${exchangeProperty.tenant}/deleteurl?throwExceptionOnFailure=false"));
+
+//        from("direct:sitemap_modify_url_lastmod")
+//                .routeId("modifyUrlRoute")
+//                .setProperty("tenant", jsonpath("$.tenant_urlset"))
+//                .process(new UrlFormatProcessor())
+//                .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.PUT))
+//                .setHeader(Exchange.CHARSET_NAME, constant("UTF-8"))
+//                .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+//                .recipientList(simple("http4://{{sitemap.service.url}}/urlsets/${exchangeProperty.tenant}?throwExceptionOnFailure=false"));
     }
 
     private List<Tenant> tenants() throws Exception {
