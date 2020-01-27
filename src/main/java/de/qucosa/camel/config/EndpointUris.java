bd095703d8f89411d5d94137817b99c1034ce92f
dseelig <daniel.seelig@slub-dresden.de>
2020-01-27 14:06:21 +0100

    Append new uris.

--- src/main/java/de/qucosa/camel/config/EndpointUris.java
+++ src/main/java/de/qucosa/camel/config/EndpointUris.java
@@ -6,4 +6,6 @@ public class EndpointUris {
     public final static String DIRECT_CREATE_URL = "direct:sitemap_create_url";
     public final static String DIRECT_DELETE_URL = "direct:sitemap_delete_url";
     public final static String SITEMAP_SERVICE_CREATE_URL = "http4://{{sitemap.service.url}}/url";
+    public final static String KAFKA_BULK_INSERT_ROUTE = "direct:bulk_insert";
+    public final static String PUSH_TO_SERVICE = "direct:push_to_service";
 }
