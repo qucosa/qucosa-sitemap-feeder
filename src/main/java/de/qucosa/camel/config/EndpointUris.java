10bea2f10ccf492789d12570c9434f324229b420
dseelig <daniel.seelig@slub-dresden.de>
2020-01-29 08:15:02 +0100

    Edit kafka pidupdate consumer uri.

--- src/main/java/de/qucosa/camel/config/EndpointUris.java
+++ src/main/java/de/qucosa/camel/config/EndpointUris.java
@@ -2,7 +2,7 @@ package de.qucosa.camel.config;
 
 public class EndpointUris {
     public final static String KAFKA_SITEMAP_CONSUMER = "kafka:service_events?groupId=sitemap&consumersCount=1&breakOnFirstError=true&autoOffsetReset=earliest";
-    public final static String KAFKA_BULK_INSERT_CONSUMER = "kafka:pidupdate?groupId=bulkinsert";
+    public final static String KAFKA_BULK_INSERT_CONSUMER = "kafka:pidupdate?groupId=bulkinsert&consumersCount=1&breakOnFirstError=true&autoOffsetReset=earliest";
     public final static String FEDORA_3_OBJECTINFO = "direct:objectinfo";
     public final static String DIRECT_CREATE_URI = "direct:sitemap_create_url";
     public final static String DIRECT_DELETE_URI = "direct:sitemap_delete_url";
