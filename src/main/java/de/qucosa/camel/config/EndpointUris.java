a1c7844cca527366d5a291195ef08411be0f2640
dseelig <daniel.seelig@slub-dresden.de>
2020-01-29 07:55:31 +0100

    Add, expand and edit endpoint uris.

--- src/main/java/de/qucosa/camel/config/EndpointUris.java
+++ src/main/java/de/qucosa/camel/config/EndpointUris.java
@@ -2,10 +2,11 @@ package de.qucosa.camel.config;
 
 public class EndpointUris {
     public final static String KAFKA_SITEMAP_CONSUMER = "kafka:service_events?groupId=sitemap&consumersCount=1&breakOnFirstError=true&autoOffsetReset=earliest";
-    public final static String FEDORA_3_OBJECTINFO = "seda:objectinfo?multipleConsumers=true";
-    public final static String DIRECT_CREATE_URL = "direct:sitemap_create_url";
-    public final static String DIRECT_DELETE_URL = "direct:sitemap_delete_url";
-    public final static String SITEMAP_SERVICE_CREATE_URL = "http4://{{sitemap.service.url}}/url";
+    public final static String KAFKA_BULK_INSERT_CONSUMER = "kafka:pidupdate?groupId=bulkinsert";
+    public final static String FEDORA_3_OBJECTINFO = "direct:objectinfo";
+    public final static String DIRECT_CREATE_URI = "direct:sitemap_create_url";
+    public final static String DIRECT_DELETE_URI = "direct:sitemap_delete_url";
     public final static String KAFKA_BULK_INSERT_ROUTE = "direct:bulk_insert";
     public final static String PUSH_TO_SERVICE = "direct:push_to_service";
+    public final static String SITEMAP_SERVICE_URI = "http4://{{sitemap.service.url}}/url";
 }
