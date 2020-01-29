125c838984b4d3a0ab3f4c6cbc3714fe90368c00
dseelig <daniel.seelig@slub-dresden.de>
2020-01-29 08:17:11 +0100

    Implement consume from pidupdate kafka topic test. Expand and edit other tests. Add cleaning the routebuilder in aftereach.

--- src/test/java/de/qucosa/camel/SitemapFeederRoutesTest.java
+++ src/test/java/de/qucosa/camel/SitemapFeederRoutesTest.java
@@ -5,21 +5,25 @@ import de.qucosa.camel.model.Url;
 import de.qucosa.camel.routebuilders.SitemapFeederRoutes;
 import de.qucosa.camel.utils.DateTimeConverter;
 import de.qucosa.data.KafkaTopicData;
+import org.apache.camel.Exchange;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.Route;
 import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.component.kafka.KafkaComponent;
 import org.apache.camel.component.kafka.KafkaConstants;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.component.properties.PropertiesComponent;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.DefaultExchange;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -30,10 +34,15 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import java.io.IOException;
 import java.util.Properties;
 
-import static de.qucosa.camel.config.EndpointUris.DIRECT_CREATE_URL;
-import static de.qucosa.camel.config.EndpointUris.DIRECT_DELETE_URL;
-import static de.qucosa.camel.config.RouteIds.APPEND_FEDORA_3_OBJ_INFO;
+import static de.qucosa.camel.config.EndpointUris.DIRECT_CREATE_URI;
+import static de.qucosa.camel.config.EndpointUris.DIRECT_DELETE_URI;
+import static de.qucosa.camel.config.EndpointUris.KAFKA_BULK_INSERT_ROUTE;
+import static de.qucosa.camel.config.EndpointUris.PUSH_TO_SERVICE;
+import static de.qucosa.camel.config.RouteIds.BULK_INSERT_APPEND_OBJ_INFO;
+import static de.qucosa.camel.config.RouteIds.BULK_INSERT_PUSH_TO_SERVICE;
 import static de.qucosa.camel.config.RouteIds.KAFKA_SITEMAP_CONSUMER_ID;
+import static de.qucosa.camel.config.RouteIds.SITEMAP_CONSUMER_APPEND_OBJ_INFO;
+import static de.qucosa.camel.config.RouteIds.SITEMAP_CONSUMER_PUSH_TO_SERVICE;
 
 @DisplayName("Test dataflow activeMQ -> kafka -> sitemap service.")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -52,9 +61,7 @@ public class SitemapFeederRoutesTest {
     @Container
     private static final KafkaContainer kafkaCon = new KafkaContainer()
             .withCreateContainerCmdModifier(
-                    createContainerCmd -> createContainerCmd.withName("qucosa-sitemap-kafka")
-
-            );
+                    createContainerCmd -> createContainerCmd.withName("qucosa-sitemap-kafka"));
 
     private static String AMQ_FILE_PATH = SitemapFeederRoutesTest.class.getResource("/jms/").getPath();
 
@@ -71,6 +78,12 @@ public class SitemapFeederRoutesTest {
         kafkaProducer = kafkaProducer();
     }
 
+    @AfterAll
+    public void shutdownAll() {
+        kafkaProducer.close();
+        kafkaCon.stop();
+    }
+
     @BeforeEach
     public void setUp() throws Exception {
         camelContext.addRoutes(new SitemapFeederRoutes());
@@ -79,75 +92,125 @@ public class SitemapFeederRoutesTest {
         kafkaComponent.setBrokers(kafkaCon.getBootstrapServers());
     }
 
+    @AfterEach
+    public void tearDown() throws Exception {
+        for (Route route : camelContext.getRoutes()) {
+            camelContext.removeRoute(route.getId());
+        }
+    }
+
     @Test
-    @Order(1)
-    @DisplayName("Ingest the sitemap url.")
-    public void ingest() throws Exception {
+    @DisplayName("Push build url object to sitemap rest service.")
+    public void pushToService() throws Exception {
         kafkaProducer.send(producerRecord(KafkaTopicData.JSON_CREATE_EVENT));
-        objState = "A";
-        eventType = "create";
 
-        camelContext.getRouteDefinition(KAFKA_SITEMAP_CONSUMER_ID).adviceWith(camelContext, urlCUD(DIRECT_CREATE_URL,"saveUrl"));
+        camelContext.getRouteDefinition(KAFKA_SITEMAP_CONSUMER_ID).adviceWith(camelContext, new AdviceWithRouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                weaveById(SITEMAP_CONSUMER_APPEND_OBJ_INFO).remove();
+                weaveById(SITEMAP_CONSUMER_PUSH_TO_SERVICE).replace().to("mock:pushToService");
+            }
+        });
 
+        MockEndpoint pushToService = camelContext.getEndpoint("mock:pushToService", MockEndpoint.class);
+        pushToService.expectedMessageCount(1);
+        camelContext.start();
+        pushToService.assertIsSatisfied();
+        camelContext.stop();
+    }
+
+    @Test
+    @DisplayName("Create the sitemap url.")
+    public void createUrl() throws Exception {
+        ProducerTemplate producerTemplate = camelContext.createProducerTemplate();
+        camelContext.getRouteDefinition("push_service").adviceWith(camelContext, urlCUD(DIRECT_CREATE_URI,"mock:saveUrl"));
         MockEndpoint saveUrl = camelContext.getEndpoint("mock:saveUrl", MockEndpoint.class);
         saveUrl.expectedMessageCount(1);
         camelContext.start();
+        producerTemplate.send(PUSH_TO_SERVICE, exchange("A", "create"));
         saveUrl.assertIsSatisfied();
         camelContext.stop();
     }
 
     @Test
-    @Order(2)
     @DisplayName("Update the sitemap url.")
     public void update() throws Exception {
-        kafkaProducer.send(producerRecord(KafkaTopicData.JSON_UPDATE_EVENT));
-        objState = "A";
-        eventType = "update";
-
-        camelContext.getRouteDefinition(KAFKA_SITEMAP_CONSUMER_ID).adviceWith(camelContext, urlCUD(DIRECT_CREATE_URL,"saveUrl"));
-
-        MockEndpoint saveUrl = camelContext.getEndpoint("mock:saveUrl", MockEndpoint.class);
-        saveUrl.expectedMessageCount(1);
+        ProducerTemplate producerTemplate = camelContext.createProducerTemplate();
+        camelContext.getRouteDefinition("push_service").adviceWith(camelContext, urlCUD(DIRECT_CREATE_URI,"mock:updateUrl"));
+        MockEndpoint updateUrl = camelContext.getEndpoint("mock:updateUrl", MockEndpoint.class);
+        updateUrl.expectedMessageCount(1);
         camelContext.start();
-        saveUrl.assertIsSatisfied();
+        producerTemplate.send(PUSH_TO_SERVICE, exchange("A", "update"));
+        updateUrl.assertIsSatisfied();
         camelContext.stop();
     }
 
     @Test
-    @Order(3)
-    @DisplayName("Delete sitemap url.")
+    @DisplayName("Delete the sitemap urls.")
     public void delete() throws Exception {
-        kafkaProducer.send(producerRecord(KafkaTopicData.JSON_DELETE_EVENT));
-        objState = "A";
-        eventType = "delete";
-
-        camelContext.getRouteDefinition(KAFKA_SITEMAP_CONSUMER_ID).adviceWith(camelContext, urlCUD(DIRECT_DELETE_URL,"deleteUrl"));
-
+        ProducerTemplate producerTemplate = camelContext.createProducerTemplate();
+        camelContext.getRouteDefinition("push_service").adviceWith(camelContext, urlCUD(DIRECT_DELETE_URI,"mock:deleteUrl"));
         MockEndpoint deleteUrl = camelContext.getEndpoint("mock:deleteUrl", MockEndpoint.class);
         deleteUrl.expectedMessageCount(1);
         camelContext.start();
+        producerTemplate.send(PUSH_TO_SERVICE, exchange("A", "delete"));
         deleteUrl.assertIsSatisfied();
         camelContext.stop();
+    }
+
+    @Test
+    @DisplayName("Create url object by PID from kafka bulkinsert consumer.")
+    public void createFromBulkInsert() throws Exception {
+        ProducerRecord<String, String> record = new ProducerRecord<>("pidupdate", 0, "qucosa:12164", "qucosa:12164");
+        kafkaProducer.send(record);
+
+        camelContext.getRouteDefinition(KAFKA_BULK_INSERT_ROUTE).adviceWith(camelContext, new AdviceWithRouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                weaveById(BULK_INSERT_APPEND_OBJ_INFO).remove();
+                weaveById(BULK_INSERT_PUSH_TO_SERVICE).replace().to("mock:pushToService");
+            }
+        });
+
+        MockEndpoint pushToService = camelContext.getEndpoint("mock:pushToService", MockEndpoint.class);
+        pushToService.expectedMessageCount(1);
+        camelContext.start();
+        pushToService.assertIsSatisfied();
+        camelContext.stop();
+    }
+
+    private Exchange exchange(String objectState, String eventType) {
+        Exchange exchange = new DefaultExchange(camelContext);
+
+        exchange.setProperty("objectState", objectState);
+        exchange.setProperty("eventType", eventType);
+        exchange.setProperty("pid", "qucosa:12164");
+
+        Url url = new Url();
+        url.setUrlSetUri("tud");
+        url.setLoc("https://tud/id/qucosa%3A12164");
+        url.setLastmod(DateTimeConverter.getCurrentW3cDatetime());
+
+        exchange.getIn().setBody(url);
+
+        return exchange;
     }
 
     private AdviceWithRouteBuilder urlCUD(String sitemapUri, String mock) {
         return new AdviceWithRouteBuilder() {
             @Override
             public void configure() throws Exception {
-                weaveById(APPEND_FEDORA_3_OBJ_INFO)
-                        .replace()
-                        .process(exchange -> {
-                            exchange.setProperty("objectState", objState);
-                            exchange.setProperty("eventType", eventType);
-                            Url url = new Url();
-                            url.setUrlSetUri("tud");
-                            url.setLoc("https://tud/id/qucosa%3A12164");
-                            url.setLastmod(DateTimeConverter.getCurrentW3cDatetime());
+                weaveByToUri(sitemapUri).replace().to(mock);
+            }
+        };
+    }
 
-                            exchange.getIn().setBody(url);
-                        });
-
-                weaveByToUri(sitemapUri).replace().to("mock:" + mock);
+    private AdviceWithRouteBuilder bulkInsertBuilder() {
+        return new AdviceWithRouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                weaveById(BULK_INSERT_APPEND_OBJ_INFO).remove();
+                weaveById(BULK_INSERT_PUSH_TO_SERVICE).replace().to("mock:bulkInsert");
             }
         };
     }
@@ -169,11 +232,5 @@ public class SitemapFeederRoutesTest {
                 0,
                 "qucosa:12164",
                 data);
-    }
-
-    @AfterAll
-    public void shutdownAll() {
-        kafkaProducer.close();
-        kafkaCon.stop();
     }
 }
