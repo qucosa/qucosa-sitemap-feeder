2448b4c3962e2b5882243166acf0d440f20da61e
dseelig <daniel.seelig@slub-dresden.de>
2020-02-26 12:27:52 +0100

    Remove namespace obj.

--- src/main/java/de/qucosa/api/UrlObjectBuilder.java
+++ src/main/java/de/qucosa/api/UrlObjectBuilder.java
@@ -35,8 +35,7 @@ public class UrlObjectBuilder {
         Url url = new Url();
 
         try {
-            String fedoraTenantName = xPath.compile("//obj:objectProfile/obj:objOwnerId/text()")
-                    .evaluate(document, XPathConstants.STRING).toString();
+            String fedoraTenantName = xPath.compile("//objectProfile/objOwnerId/text()").evaluate(document, XPathConstants.STRING).toString();
             Tenant tenant = tenant(fedoraTenantName);
 
             url.setUrlSetUri(tenant.getSmall());
@@ -51,7 +50,7 @@ public class UrlObjectBuilder {
 
     public String objectState() {
         try {
-            return xPath.compile("//obj:objectProfile/obj:objState/text()").evaluate(document, XPathConstants.STRING).toString();
+            return xPath.compile("//objectProfile/objState/text()").evaluate(document, XPathConstants.STRING).toString();
         } catch (XPathExpressionException e) {
             throw new RuntimeException("Object state is not exist in document.", e);
         }
